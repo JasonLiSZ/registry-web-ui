28e64ce0a2c99a4645ada796564a656feece5125
Jason <JasonLiSz@hotmail.com>
2017-10-05 14:57:38 +0800

    introduce swing template

--- web/app.js
+++ web/app.js
@@ -1,1 +1,55 @@
-console.log('start web');
+const render = require('./lib/render');
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const koaBody = require('koa-body');
+
+const Koa = require('koa');
+const app = module.exports = new Koa();
+
+app.use(logger());
+app.use(render);
+app.use(koaBody());
+
+router.get('/', fnIndex)
+  .get('/image/', fnListImage)
+  .get('/image/:image/tag/', fnListImageTag)
+  .get('/image/:image/tag/:tag/head/', fnGetImageTagHead)
+  .get('/image/:image/tag/:tag/body/', fnGetImageTagBody)
+  .delete('/image/:image/digest/:digest/', fnDeleteImageTag);
+
+app.use(router.routes());
+
+async function fnIndex(ctx) {
+  await ctx.render('index', {
+    posts: ['a', 'b', 'x', 'y']
+  });
+}
+
+async function fnListImage(ctx) {
+
+}
+
+async function fnListImageTag(ctx) {
+
+}
+async function fnGetImageTagHead(ctx) {
+
+}
+async function fnGetImageTagBody(ctx) {
+
+}
+async function fnDeleteImageTag(ctx) {
+
+}
+
+async function create(ctx) {
+  const post = ctx.request.body;
+  const id = posts.push(post) - 1;
+  post.created_at = new Date();
+  post.id = id;
+  ctx.redirect('/');
+}
+
+app.listen(4000, function () {
+  console.log('App listening on port 4000!');
+});
