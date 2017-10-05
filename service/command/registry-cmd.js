a21d228931bb6c374aed2bbfb09a9dc13abe3784
Jason <JasonLiSz@hotmail.com>
2017-10-05 12:32:56 +0800

    use async await

--- service/command/registry-cmd.js
+++ service/command/registry-cmd.js
@@ -3,38 +3,47 @@ const child_process = require('child_process');
 const util = require('util');
 
 class RegistryCmd {
-  listImage(paras, callback) {
+  listImage(paras) {
     let cmd = util.format('curl -X GET -u %s:%s https://%s/v2/_catalog', paras.user, paras.password, paras.registry);
-    this.exec(cmd, callback);
+    return this.exec(cmd);
   }
 
-  listImageTag(paras, callback) {
+  listImageTag(paras) {
     let cmd = util.format('curl -X GET -u %s:%s https://%s/v2/%s/tags/list', paras.user, paras.password, paras.registry, paras.image);
-    this.exec(cmd, callback);
+    return this.exec(cmd);
   }
 
-  getImageTagHead(paras, callback) {
+  getImageTagHead(paras) {
     let cmd = util.format('curl -H "Accept:application/vnd.docker.distribution.manifest.v2+json" GET -vvv -k -u %s:%s https://%s/v2/%s/manifests/%s', paras.user, paras.password, paras.registry, paras.image, paras.tag);
-    this.exec(cmd, callback);
+    return this.exec(cmd);
   }
 
-  getImageTagBody(paras, callback) {
+  getImageTagBody(paras) {
     let cmd = util.format('curl -X GET -u %s:%s https://%s/v2/%s/manifests/%s', paras.user, paras.password, paras.registry, paras.image, paras.tag);
-    this.exec(cmd, callback);
+    return this.exec(cmd);
   }
 
-  deleteImageTag(paras, callback) {
+  deleteImageTag(paras) {
     let cmd = util.format('curl -X DELETE -u %s:%s https://%s/v2/%s/manifests/%s', paras.user, paras.password, paras.registry, paras.image, paras.digest);
-    this.exec(cmd, callback);
+    return this.exec(cmd);
   }
 
-  exec(cmd, callback) {
+  exec(cmd) {
     console.log(cmd);
 
+    return new Promise((resolve, reject) =>{
+      child_process.exec(cmd, function(err, stdout, stderr){
+        if(err) reject(err);
+        else resolve(stdout);
+      });
+    });
+
+    /*
     var resp = child_process.execSync(cmd);
     var result = resp.toString('UTF8');
 
     callback(result);
+    */
   }
 
 
--- service/webapi/registry-api.js
+++ service/webapi/registry-api.js
@@ -6,25 +6,15 @@ var fnListImage = async(ctx, next) => {
   let user = ctx.request.header.user;
   let password = ctx.request.header.password;
 
-  let result = cmd.listImage({
-    'registry': registry,
-    'user': user,
-    'password': password
-  }, function (result) {
-    try {
-      let data = JSON.parse(result);
-
-      if (data.errors && data.errors.length > 0) {
-        ctx.response.body = data.errors[0].message;
-        ctx.response.status = 500;
-        return;
-      }
-
-      ctx.response.body = data;
-    } catch (ex) {
-      ctx.throw(ex.message, 500);
-    }
-  });
+  await cmd.listImage({
+      'registry': registry,
+      'user': user,
+      'password': password
+    }).then(result => ctx.response.body = result)
+    .catch(err => {
+      ctx.response.body = err.message;
+      ctx.response.status = 500;
+    });
 };
 
 var fnListImageTag = async(ctx, next) => {
@@ -34,26 +24,16 @@ var fnListImageTag = async(ctx, next) => {
 
   let image = ctx.params.image;
 
-  let result = cmd.listImageTag({
-    'registry': registry,
-    'user': user,
-    'password': password,
-    'image': image
-  }, function (result) {
-    try {
-      let data = JSON.parse(result);
-
-      if (data.errors && data.errors.length > 0) {
-        ctx.response.body = data.errors[0].message;
-        ctx.response.status = 500;
-        return;
-      }
-
-      ctx.response.body = data;
-    } catch (ex) {
-      ctx.throw(ex.message, 500);
-    }
-  });
+  await cmd.listImageTag({
+      'registry': registry,
+      'user': user,
+      'password': password,
+      'image': image
+    }).then(result => ctx.response.body = result)
+    .catch(err => {
+      ctx.response.body = err.message;
+      ctx.response.status = 500;
+    });
 };
 
 var fnGetImageTagHead = async(ctx, next) => {
@@ -64,26 +44,17 @@ var fnGetImageTagHead = async(ctx, next) => {
   let image = ctx.params.image;
   let tag = ctx.params.tag;
 
-  let result = cmd.getImageTagHead({
-    'registry': registry,
-    'user': user,
-    'password': password,
-    'image': image,
-    'tag': tag
-  }, function (result) {
-    //Docker-Content-Digest: sha256:467af83b77759844196762fc8d9e981a0c3d4fa3c59d76445b7e35a6ee5ca916
-    var matches = result.match(/Docker-Content-Digest: .*/);
-    if (matches && matches.length == 1) {
-      var sha256 = matches[0].replace(/Docker-Content-Digest: /, '');
-      ctx.response.body = {
-        'digest': sha256
-      };
-      return;
-    }
-
-    ctx.response.body = 'not found'
-    ctx.response.status = 4040;
-  });
+  await cmd.getImageTagHead({
+      'registry': registry,
+      'user': user,
+      'password': password,
+      'image': image,
+      'tag': tag
+    }).then(result => ctx.response.body = result)
+    .catch(err => {
+      ctx.response.body = err.message;
+      ctx.response.status = 500;
+    });
 };
 
 var fnGetImageTagBody = async(ctx, next) => {
@@ -94,27 +65,17 @@ var fnGetImageTagBody = async(ctx, next) => {
   let image = ctx.params.image;
   let tag = ctx.params.tag;
 
-  let result = cmd.getImageTagBody({
-    'registry': registry,
-    'user': user,
-    'password': password,
-    'image': image,
-    'tag': tag
-  }, function (result) {
-    try {
-      let data = JSON.parse(result);
-
-      if (data.errors && data.errors.length > 0) {
-        ctx.response.body = data.errors[0].message;
-        ctx.response.status = 500;
-        return;
-      }
-
-      ctx.response.body = data;
-    } catch (ex) {
-      ctx.throw(ex.message, 500);
-    }
-  });
+  await cmd.getImageTagBody({
+      'registry': registry,
+      'user': user,
+      'password': password,
+      'image': image,
+      'tag': tag
+    }).then(result => ctx.response.body = result)
+    .catch(err => {
+      ctx.response.body = err.message;
+      ctx.response.status = 500;
+    });
 };
 
 var fnDeleteImageTag = async(ctx, next) => {
@@ -125,32 +86,17 @@ var fnDeleteImageTag = async(ctx, next) => {
   let image = ctx.params.image;
   let digest = ctx.params.digest;
 
-  let result = cmd.deleteImageTag({
-    'registry': registry,
-    'user': user,
-    'password': password,
-    'image': image,
-    'digest': digest
-  }, function (result) {
-    try {
-      if (result == '') {
-        ctx.response.body = 'delete success';
-        return;
-      }
-
-      let data = JSON.parse(result);
-
-      if (data.errors && data.errors.length > 0) {
-        ctx.response.body = data.errors[0].message;
-        ctx.response.status = 500;
-        return;
-      }
-
-      ctx.response.body = data;
-    } catch (ex) {
-      ctx.throw(ex.message, 500);
-    }
-  });
+  await cmd.deleteImageTag({
+      'registry': registry,
+      'user': user,
+      'password': password,
+      'image': image,
+      'digest': digest
+    }).then(result => ctx.response.body = result)
+    .catch(err => {
+      ctx.response.body = err.message;
+      ctx.response.status = 500;
+    });
 }
 
 module.exports = {
